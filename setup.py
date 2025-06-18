a6d6068ae42e1ebf5b2cd4a009af9c9baab7f14f
Phil <payoung97@outlook.com>
2025-06-17 20:27:53 -0500

    This includes partial EDA

--- setup.py
+++ setup.py
@@ -1,32 +1,24 @@
-from pkg_resources import Requirement
-from setuptools import find_packages,setup
 from typing import List
+from setuptools import setup, find_packages
 
-hyphen_dot='-e .'
-
-def get_requirements(file_path:str)->List[str]:
+def get_requirements(file_path: str) -> List[str]:
     '''
-    This function will return the list of requriements
+    Reads the requirements from a file and returns them as a list.
+    Removes '-e .' if present.
+    '''
+    with open(file_path, 'r') as file_obj:
+        requirements = file_obj.readlines()
 
-'''
-    requirements=[]
-    with open(file_path) as file_obj:
-    requirements=file_obj.readlines()
-    requirements=[req.replace("\n","") for req in requirements]
+    requirements = [req.strip() for req in requirements]
 
-    if hyphen_dot in requirements:
-        requirements.remove(hyphen_dot)
-return requirements
+    if '-e .' in requirements:
+        requirements.remove('-e .')
 
+    return requirements
 
 setup(
-name='MLProject1',
-version='0.0.1',
-author='Phil',
-author_email='payoung97@outlook.com',
-packages=find_packages(),
-install_requires=get_requirements('requirements.txt')
-
-
-
+    name='your_package_name',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt'),
 )
